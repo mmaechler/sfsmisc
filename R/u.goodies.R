696785d8ad627f1c85210c075a9e302a5e5d9854
Martin Maechler <maechler@r-project.org>
1999-07-07 17:31:24 +0000

    version of 1999.07.07.17.31.24 (now is 09 Aug 2014).

--- R/u.goodies.R
+++ R/u.goodies.R
@@ -1,12 +1,24 @@
-if(is.R())
+####--- Utilities -----------------
+
+if(is.R()) {
     u.assign0 <- function(x, value, immediate = FALSE) {
         ## Purpose: Simple function with identical UI for both R & S
         ## Author: Martin Maechler, Date: 7 Jul 1999
         assign(x, value, envir = .GlobalEnv)
     }
-else # S-plus
+    u.get0 <- function(x) get(x, envir = .GlobalEnv)
+    u.sys <- function(..., intern=TRUE)
+      system(paste(..., sep=""), intern=intern)
+    
+} else { # S-plus
+
     u.assign0 <- function(x, value, immediate = FALSE) {
         ## Purpose: Simple function with identical UI for both R & S
         ## Author: Martin Maechler, Date: 7 Jul 1999
         assign(x, value, frame = 0, immediate = immediate)
     }
+    u.get0 <- function(x) get(x, frame = 0)
+    u.sys <- function(..., intern=TRUE)
+      unix(paste(..., sep=""), output.to.S = intern)
+}
+
