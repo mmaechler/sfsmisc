75b1b333cd64249a8ed3cb835dca1108baab1d17
Martin Maechler <maechler@r-project.org>
2000-03-07 18:04:58 +0000

    version of 2000.03.07.18.04.58 (now is 09 Aug 2014).

--- R/p.ts.R
+++ R/p.ts.R
@@ -1,5 +1,6 @@
 p.ts <- function(x, nrplots = max(1, min(8, n%/%400)), overlap = nk %/% 16,
-                 main.tit=NULL, quiet = FALSE, ...)
+                 main.tit = NULL, ylim = NULL, ylab = "",
+                 quiet = FALSE, mgp = c(1.25, .5, 0), ...)
 {
   ## Purpose: plot.ts with multi-plots + Auto-Title -- currently all on 1 page
   ## -------------------------------------------------------------------------
@@ -16,25 +17,29 @@ p.ts <- function(x, nrplots = max(1, min(8, n%/%400)), overlap = nk %/% 16,
 
   if(is.null(main.tit)) main.tit <- paste(deparse(substitute(x)))
   n <- length(x)
-  if(nrplots == 1) plot.ts(x, ..., main = main.tit)
-  else {
-    if(nrplots <= 0) return(nrplots)
-    if(n<=1) stop("'x' must have at least two points!")
-    do.dates <- !is.null(class(x)) && class(x) == "cts"
-    if(do.dates) x <- as.rts(x)# dates() as below fails [S+ 3.4]
+  if(nrplots == 1)
+    plot.ts(x, ..., main = main.tit, ylim = ylim, ylab = ylab)
+  else if(nrplots <= 0)
+    return(nrplots)
+  else { # nrplots >= 2 :
+    if(n <= 1) stop("'x' must have at least two points!")
+    ##-  do.dates <- !is.null(class(x)) && class(x) == "cts"
+    ##-  if(do.dates) x <- as.rts(x)# dates() as below fails [S+ 3.4]
     scal <- (end(x) - (t1 <- start(x)))/(n-1)
     nk <- n %/% nrplots
-    yl <- range(pretty(range(x, na.rm = TRUE)))
-    pp <- mult.fig(mfrow=c(nrplots,1), main=main.tit, quiet = TRUE)
+    if(is.null(ylim))
+       ylim <- range(pretty(range(x, na.rm = TRUE)))
+    pp <- mult.fig(mfrow=c(nrplots,1), main= main.tit, quiet= TRUE, mgp=mgp, marP= c(-1,-1,-2,0))
+    ##    --------
     on.exit(par(pp $ old.par))
     
     for(i in 1:nrplots) {
-      i0   <- max(0, (-overlap+(i-1)*nk)-1)
+      i0  <- max(0, (-overlap + (i-1)*nk)-1)
       in1 <- min(n, i*nk + overlap)-1
       st <- t1 + scal*i0 ##;  if(do.dates) st <- dates(st)
       en <- t1 + scal*in1##; if(do.dates) en <- dates(en)
       if(!quiet) cat(i," -- start= ", format(st), "; end  =", format(en),"\n")
-      plot.ts(window(x, start= st, end  = en), ylim= yl, ...)
+      plot.ts(window(x, start= st, end  = en), ylim = ylim, ylab = ylab, ...)
     }
   }
 }
