5e9ec71176bada8af8456698e9eb2249513181e3
Martin Maechler <maechler@r-project.org>
2001-04-27 17:10:28 +0000

    version of 2001.04.27.17.10.28 (now is 09 Aug 2014).

--- R/p.ts.R
+++ R/p.ts.R
@@ -16,30 +16,33 @@ p.ts <- function(x, nrplots = max(1, min(8, n%/%400)), overlap = nk %/% 16,
   ## Author: Martin Maechler, Date:  1 Jul 1994; 18.Dec,1998.
 
   if(is.null(main.tit)) main.tit <- paste(deparse(substitute(x)))
-  n <- length(x)
+  isMat <- is.matrix(x)
+  n <- if(isMat) nrow(x) else length(x)
   if(nrplots == 1)
     plot.ts(x, ..., main = main.tit, ylim = ylim, ylab = ylab)
   else if(nrplots <= 0)
     return(nrplots)
   else { # nrplots >= 2 :
-    if(n <= 1) stop("'x' must have at least two points!")
+    if(n <= 1) stop("`x' must have at least two points!")
+    if(!is.ts(x)) x <- as.ts(x)
     ##-  do.dates <- !is.null(class(x)) && class(x) == "cts"
     ##-  if(do.dates) x <- as.rts(x)# dates() as below fails [S+ 3.4]
     scal <- (end(x) - (t1 <- start(x)))/(n-1)
     nk <- n %/% nrplots
     if(is.null(ylim))
        ylim <- range(pretty(range(x, na.rm = TRUE)))
-    pp <- mult.fig(mfrow=c(nrplots,1), main= main.tit, quiet= TRUE, mgp=mgp, marP= c(-1,-1,-2,0))
     ##    --------
+    pp <- mult.fig(mfrow=c(nrplots,1), main = main.tit, quiet= TRUE,
+                   mgp = mgp, marP = c(-1,-1,-2,0))
     on.exit(par(pp $ old.par))
-    
     for(i in 1:nrplots) {
       i0  <- max(0, (-overlap + (i-1)*nk)-1)
       in1 <- min(n, i*nk + overlap)-1
-      st <- t1 + scal*i0 ##;  if(do.dates) st <- dates(st)
+      st <- t1 + scal*i0 ##; if(do.dates) st <- dates(st)
       en <- t1 + scal*in1##; if(do.dates) en <- dates(en)
       if(!quiet) cat(i," -- start= ", format(st), "; end  =", format(en),"\n")
-      plot.ts(window(x, start= st, end  = en), ylim = ylim, ylab = ylab, ...)
+      plot(window(x, start= st, end = en), ylim = ylim, ylab = ylab, ...,
+           plot.type = "single") # plot.type : for plot.mts only
     }
   }
 }
