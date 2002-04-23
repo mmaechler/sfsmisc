0cd7e029a139128a2a35ca8d5b31ddf3121c4fdc
Martin Maechler <maechler@r-project.org>
2002-04-23 10:28:58 +0000

    version of 2002.04.23.10.28.58 (now is 09 Aug 2014).

--- R/mult.fig.R
+++ R/mult.fig.R
@@ -1,19 +1,18 @@
 mult.fig <-
 function(nr.plots, mfrow, mfcol,
-         marP = rep(0,4), mgp = c(1.5,.6,0),
-         mar = marP + .1 + c(4,4,2,1),
-         main = NULL,
-         tit.wid = if (is.null(main)) 0 else 1 + 1.5*cex.main,
+         marP = rep(0, 4), mgp = c(1.5, 0.6, 0),
+         mar = marP + 0.1 + c(4,4,2,1), oma = c(0,0, tit.wid, 0),
+         main = NULL, tit.wid = if (is.null(main)) 0 else 1 + 1.5*cex.main,
          quiet = .Device == "postscript",
-         cex.main = par("cex.main"),
+         cex.main = par("cex.main"), line.main = cex.main - 1/2,
          col.main = par("col.main"),
          font.main = par("font.main"),
          ...)
 {
-  ## Purpose: 'MULTiple FIGures' incl. TITLE and other good defaults
+  ## Purpose: 'MULTiple FIGures' incl. title and other good defaults
   ## -------------------------------------------------------------------------
-  ## Arguments: 
-  ##             -- Either ONE of the first 3 arguments --
+  ## Arguments: -- Either ONE of the first 3 arguments --
+  ###  =========> help(mult.fig)
   ## -------------------------------------------------------------------------
   ## Author: Martin Maechler, 1990 (UW, Seattle) -- 1995
   ## -------------------------------------------------------------------------
@@ -25,7 +24,6 @@ function(nr.plots, mfrow, mfcol,
         stop("must either specify 'nr.plots', 'mfrow' or 'mfcol' !")
       else  mfrow <- n2mfrow (nr.plots)
     }
-  oma <- c(0, 0, tit.wid, 0)
   old.par <<-
     if(use.row) par(mfrow = mfrow, oma= oma, mar = mar, mgp= mgp)
     else        par(mfcol = mfcol, oma= oma, mar = mar, mgp= mgp)
@@ -36,7 +34,7 @@ function(nr.plots, mfrow, mfcol,
   if (!is.null(main)) {# Do title *before* first plot!
       if(is.R()) plot.new()
       mtext(main, side = 3, outer = TRUE,
-            line = cex.main, # was tit.wid - 4,
+            line = line.main,
             cex = cex.main,
             font = font.main, col = col.main, ...)
       if(is.R()) par(new=TRUE)# reverse `plot.new()' above
