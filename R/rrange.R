ced90df7539a785533a126b8297457411093fbc9
Martin Maechler <maechler@r-project.org>
2003-06-10 14:32:29 +0000

    version of 2003.06.10.14.32.29 (now is 09 Aug 2014).

--- R/rrange.R
+++ R/rrange.R
@@ -6,11 +6,11 @@ rrange <- function(x, range = 1, coef = 1.5)
     if(!missing(coef)) stop("Must use either `range' or `coef'")
     coef <- 1.5 * range
   }
-  ## S: (boxplot(..., plot = FALSE)$stats)[c(5, 1)]
   ## Work around R 1.2.3 buglet :
   do <- 123 == 100 * as.numeric(R.version$major) +
                 10 * as.numeric(R.version$minor)
 
+  ## S: (boxplot(..., plot = FALSE)$stats)[c(5, 1)]
   boxplot.stats(x, coef=coef, do.conf = FALSE,
                 do.out = do)$stats[c(1,5)]
 }
