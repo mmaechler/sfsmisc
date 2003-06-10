4adfab40f020fb9812a56857f3bcef46c11ca37d
Martin Maechler <maechler@r-project.org>
2003-06-10 14:55:56 +0000

    version of 2003.06.10.14.55.56 (now is 09 Aug 2014).

--- R/rrange.R
+++ R/rrange.R
@@ -1,4 +1,4 @@
-rrange <- function(x, range = 1, coef = 1.5)
+rrange <- function(x, range = 1, coef = 1.5, na.rm = TRUE)
 {
   ## Purpose: `Robust RANGE', ===>  ?rrange
   ## Author: Martin Maechler, 1990
@@ -9,6 +9,8 @@ rrange <- function(x, range = 1, coef = 1.5)
   ## Work around R 1.2.3 buglet :
   do <- 123 == 100 * as.numeric(R.version$major) +
                 10 * as.numeric(R.version$minor)
+  if(!na.rm && any(is.na(x)))
+      return(0+ c(NA,NA))# numeric NA
 
   ## S: (boxplot(..., plot = FALSE)$stats)[c(5, 1)]
   boxplot.stats(x, coef=coef, do.conf = FALSE,
