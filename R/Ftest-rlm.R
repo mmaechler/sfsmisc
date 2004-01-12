d6a4d3988d2d1af5e0728240134070f3bfdcb90c
Martin Maechler <maechler@r-project.org>
2004-01-12 19:14:54 +0000

    version of 2004.01.12.19.14.54 (now is 09 Aug 2014).

--- R/Ftest-rlm.R
+++ R/Ftest-rlm.R
@@ -3,48 +3,49 @@
 ##- Subject: robuster F-test
 ##- Date: Fri, 14 Jul 2000 17:01:55 +0200 (CEST)
 
-
 f.robftest <- function(object, var = -1)
 {
-  ## Purpose:   robust F-test: Wald test for several coefficients of
-  ##            an rlm object
-  ## -------------------------------------------------------------------------
-  ## Arguments:
-  ##   object   result of rlm(...)
-  ##   var      variables. Either their names or their indices
-  ## -------------------------------------------------------------------------
-  ## Author: Werner Stahel, Date: 14 Jul 2000;  MM, 2000-07-14
+    ## Purpose:   robust F-test: Wald test for several coefficients of
+    ##            an rlm object
+    ## -------------------------------------------------------------------------
+    ## Arguments:
+    ##   object   result of rlm(...)
+    ##   var      variables. Either their names or their indices
+    ##		  Default: all *but* the intercept
+    ## -------------------------------------------------------------------------
+    ## Author: Werner Stahel, Date: 14 Jul 2000;  MM, 2000-07-14
 
-  if ( !inherits(object,"rlm"))
-    stop("f.robftest only works for rlm objects")
+    if (!inherits(object,"rlm"))
+        stop("f.robftest() only works for 'rlm' objects")
 
-  require("MASS")# for summary.rlm()
+    require("MASS")# for summary.rlm()
 
-  ## determine and check coefficients to be tested
-  t.cf <- object$coef
-  t.vind <-
-    if(is.character(var)) match(var,names(t.cf)) else seq(length(t.cf))[var]
-  t.wrong <- is.na(t.vind) | t.vind > length(t.cf) | t.vind < 1
-  if (any(t.wrong)) 
-    stop(paste("variable ",var[t.wrong]," not found"))
-  t.coef <- t.cf[t.vind]
-  if (0 == (t.nv <- length(t.coef)))
-    stop("no variables to be tested")
-  ## covariance matrix of estimated coefficients
-  t.r <- summary.rlm(object, method="XtWX")
-  ## Nota BENE:  vcov() calls vcov.lm() which uses  $sigma instead of $stddev !
-  t.cov <- t.r$cov.unscaled[t.vind,t.vind] * t.r$stddev ^ 2
-  ## results
+    ## determine and check coefficients to be tested
+    cf <- object$coef
+    iind <- if(is.character(var)) match(var,names(cf)) else seq(length(cf))[var]
+    wrong <- is.na(iind) | iind > length(cf) | iind < 1
+    if (any(wrong))
+        stop(paste("variable ",var[wrong]," not found"))
+    cf <- cf[iind]
+    if (0 == (t.nv <- length(cf)))
+        stop("no variables to be tested")
+    ## covariance matrix of estimated coefficients: calls summary.rlm():
+    t.r <- summary(object, method="XtWX")
+    ## Nota BENE: vcov() calls vcov.lm() which uses $sigma instead of $stddev !
+    t.cov <- t.r$cov.unscaled[iind,iind] * t.r$stddev ^ 2
 
-  ##t.f <- c(t.coef %*% solve(t.cov) %*% t.coef)/t.nv
-  ## quite a bit more efficient (for larger p):  x' A^-1 x :
-  t.f <- sum(t.coef * solve(t.cov, t.coef))/t.nv
-  t.df <- c(t.nv, t.r$df[2])
+    ## Instead of   c(cf %*% solve(t.cov) %*% cf)/t.nv
+    ## quite a bit more efficient (for larger p):  x' A^-1 x :
+    t.f <- sum(cf * solve(t.cov, cf))/t.nv
+    df <- c(t.nv, t.r$df[2])
 
-  ## MM: FIXME:  Return an object of class "htest" ! ---> print.* method, etc
-  ## ---------   library(ctest)
-  ##             --------------
-  list(statistic = c(F = t.f), df = t.df,
-       p.value = pf(t.f, t.df[1], t.df[2], lower.tail = FALSE))
+    ## MM: Return an object of class "htest"  ---> nice print.*() method !
+    structure(list(statistic = c(F = t.f), df = df,
+                   data.name = paste("from", deparse(object$call)),
+                   method = "robust F-test (as if non-random weights)",
+                   alternative = "two.sided",
+                   null.value = {c0 <- cf; c0[] <- 0; c0},
+                   p.value = pf(t.f, df[1], df[2], lower.tail = FALSE)),
+              class = "htest")
 }
 
