c27d39a927ac0d16205fe94103c2228d379865a1
Martin Maechler <maechler@r-project.org>
2004-07-29 19:20:16 +0000

    version of 2004.07.29.19.20.16 (now is 24 Jul 2007).

--- demo/prime-numbers.R
+++ demo/prime-numbers.R
@@ -6,9 +6,30 @@
 ###--- load the current function definitions:
 ##source("/u/maechler/R/SOURCE-ME")
 ##included above:
-source("/u/maechler/S/MISC/prime-numbers-fn.S")
+source("/u/maechler/R/MM/MISC/prime-numbers-fn.R")
 
-if(is.R()) unix.time <- system.time
+if(!is.R()) system.time <- system.time
+
+### 1) The super speedy primes() function from Bill Venables
+###    {and improved by M.Maechler}:
+
+## on a Pentium 4 2.80 GHz with 2 GB RAM :
+
+N <- 1e7
+
+for(i in 1:3) { gc(); print(system.time(p7 <- primes.(N))[1:3]) }
+##- [1] 3.86 1.93  8.75
+##- [1] 4.02 1.60 11.34
+##- [1] 4.14 1.60 11.51
+
+for(i in 1:3) { gc(); print(system.time(p7. <- primes(N))[1:3]) }
+##- [1] 2.29 0.76 6.47
+##- [1] 2.58 0.73 6.67
+##- [1] 2.71 0.59 6.64
+stopifnot(all(p7 == p7.))
+
+
+## 2)
 
 factorize(n <- c(7,27,37,5*c(1:5, 8, 10)))
 factorize(47)
@@ -19,21 +40,22 @@ factorize(131301607)# prime -> still quick!
 ## should be much quicker with other algo (2nd largest prime == 71) !!
 factorize(76299312910)
 
-unix.time(fac.1ex <- factorize(1000 + 1:99)) #-- 0.95 sec (sophie Sparc 5)
+system.time(fac.1ex <- factorize(1000 + 1:99)) #-- 0.95 sec (sophie Sparc 5)
 #-- 0.02 sec (P 4, 1.6GHz);  0.4 / .65 sec (florence Ultra 1/170)
-unix.time(fac.2ex <- factorize(10000 + 1:999))
+system.time(fac.2ex <- factorize(10000 + 1:999))
 ## R 0.49 : 5.4 sec (florence Ultra 1/170)
 ## ------   6.1 sec (sophie   Ultra 1/140)
 ## R 0.50-: ~ 3.5 sec (sophie  ..........)  <<< FASTER !
 ## ------
 
 ## This REALLY takes time -- (for loop over 10000; 10000 times res[[i]] <-..
-unix.time(factorize.10000 <- factorize(1:10000))
+system.time(factorize.10000 <- factorize(1:10000))
 ## sophie: Sparc 5 (..) :lots of swapping after while, >= 20 minutes CPU;
 ##			then using less and less CPU, ..more swapping ==> KILL
 ## florence (hypersparc): [1] 1038.90   5.09 1349.   ( 17 min. CPU)
 ## lynne (Ultra-1):       [1]  658.77   0.90  677.
 ## lynne (Pentium 4):     [1]    2.43   0.16    2.68
+## helen (Pentium 4), R1.9.1:    1.02   0.01    1.04
 
 object.size(factorize.10000) #-->[1] 3027743 now (R 1.5.1) 3188928
 
@@ -44,7 +66,7 @@ which(!test.factorize(factorize(8000 + 1:1000)))
 
 
 prime.sieve(prime.sieve())
-unix.time(P1e4 <- prime.sieve(prime.sieve(prime.sieve()), max=10000))
+system.time(P1e4 <- prime.sieve(prime.sieve(prime.sieve()), max=10000))
 ##-> 1.45 (on sophie: fast Sparc 5 ..)
 ##-> ~0.8 (on jessica: Ultra-2)
 ##->  0.08 (on lynne, Pentium 4 (1600 MHz))
@@ -53,18 +75,18 @@ stopifnot(length(P1e4) == 1229)
 
 CPU.p1e4 <- numeric(20)
 for(i in 1:20)  CPU.p1e4[i] <-
-  unix.time(P1e4 <- prime.sieve(prime.sieve(prime.sieve()), max=10000))[1]
+  system.time(P1e4 <- prime.sieve(prime.sieve(prime.sieve()), max=10000))[1]
 CPU.p1e4
 summary(CPU.p1e4)
 ##-Ultra-2    Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
 ##-Ultra-2   0.690   0.690   0.790   0.755   0.800   0.810
 ## P4 R-?)   0.070   0.070   0.080   0.078   0.080   0.100
 ## P4 R-1.9  0.040   0.050   0.050   0.048   0.050   0.050
-unix.time(P1e4.2 <- prime.sieve( max=10000))
+system.time(P1e4.2 <- prime.sieve( max=10000))
 ##-> 1.46 (sophie)   maybe a little longer
 stopifnot(identical(P1e4 , P1e4.2))
 
-unix.time(P1e5 <- prime.sieve(P1e4, max=1e5))
+system.time(P1e5 <- prime.sieve(P1e4, max=1e5))
 ##-> 105.7  (on sophie: fast Sparc 5)
 ##->  58.83 (on jessica: Ultra2)
 ##->   5.67 (on lynne: Pentium 4)
