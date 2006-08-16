c36d33c5c4573c2ac99e403098a40600dcde8ccd
Martin Maechler <maechler@r-project.org>
2006-08-16 20:55:56 +0000

    version of 2006.08.16.20.55.56 (now is 09 Aug 2014).

--- R/rrange.R
+++ R/rrange.R
@@ -3,16 +3,12 @@ rrange <- function(x, range = 1, coef = 1.5, na.rm = TRUE)
   ## Purpose: `Robust RANGE', ===>  ?rrange
   ## Author: Martin Maechler, 1990
   if(!missing(range)) {
-    if(!missing(coef)) stop("Must use either `range' or `coef'")
+    if(!missing(coef)) stop("Must use either 'range' or 'coef'")
     coef <- 1.5 * range
   }
-  ## Work around R 1.2.3 buglet :
-  do <- 123 == 100 * as.numeric(R.version$major) +
-                10 * as.numeric(R.version$minor)
   if(!na.rm && any(is.na(x)))
       return(0+ c(NA,NA))# numeric NA
 
   ## S: (boxplot(..., plot = FALSE)$stats)[c(5, 1)]
-  boxplot.stats(x, coef=coef, do.conf = FALSE,
-                do.out = do)$stats[c(1,5)]
+  boxplot.stats(x, coef = coef, do.conf= FALSE, do.out= FALSE)$stats[c(1,5)]
 }
