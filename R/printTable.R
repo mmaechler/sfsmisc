cfc144cc32f1606667258a9d8d0d12afee3419b6
Martin Maechler <maechler@r-project.org>
2010-10-27 08:28:22 +0000

    improvements (*named* dimnames) -- comment about  zero.print

--- R/printTable.R
+++ R/printTable.R
@@ -40,26 +40,35 @@ printTable2 <- function(table2, digits = 3)
 ### The original catCon() function did compute and print;
 ### now separated :
 
-margin2table <- function(x, totName = "sum") {
+margin2table <- function(x, totName = "sum", name.if.empty=FALSE) {
     x <- as.matrix(x)
     r <- rowSums(x)
     r <- rbind(cbind(x, r), c(colSums(x), sum(r)))
     dimnames(r) <-
-        if(!is.null(dnx <- dimnames(x))) {
-            list(if(!is.null(dnx[[1]])) c(dnx[[1]], totName),
-                 if(!is.null(dnx[[2]])) c(dnx[[2]], totName))
-        } ## else NULL
-    class(r) <- "margin2table"
+	if(!is.null(dnx <- dimnames(x))) {
+	    dn <- list(if(!is.null(dnx[[1]])) c(dnx[[1]], totName),
+		       if(!is.null(dnx[[2]])) c(dnx[[2]], totName))
+	    names(dn) <- names(dnx)
+	    dn
+	} else if(name.if.empty) {
+	    list(c(rep("", nrow(x)), totName),
+		 c(rep("", ncol(x)), totName))
+	}
+    class(r) <- c("margin2table", "table")
     r
 }
 
-print.margin2table <- function(x, digits = 3, ...)
+print.margin2table <- function(x, digits = 3, quote = FALSE, right = TRUE, ...)
 {
   if(is.null(d <- dim(x)) || length(d <- d - 1) !=2)
       stop("'x' is not a matrix")
-  N <- d[1];  M <- d[2]
-  cx <- format(round(x, digits))
-  cx <- cbind(rbind(cx, paste(rep("-", max(nchar(cx))), collapse = "")), "|")
-  print(cx[c(1:N,N+2,N+1), c(1:M,M+2,M+1)], quote = FALSE)
+  N <- d[1]; M <- d[2]
+  cx <- format(round(x, digits))[c(1:N,N+1,N+1), c(1:M,M+1,M+1)]
+  cx[N+1,] <- rownames(cx)[N+1] <- "--"
+  cx[,M+1] <- colnames(cx)[M+1] <- "|"
+  ## TODO: think of implementing  zero.print = "." -- as in print.table()
+  ## TODO(2): improve that in print.table(),
+  ## (e.g. replace "0.0" or "0e0" by ".  ";  "00.0" by " .  ")
+  print(cx, quote=quote, right=right, ...)
   invisible(x)
 }
