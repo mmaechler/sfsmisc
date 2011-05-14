2e27a5edc1396041b588fc884ffb2db6905cfd98
Martin Maechler <maechler@r-project.org>
2011-05-14 20:13:41 +0000

    Initial revision

---
+++ R/approx2.R
@@ -0,0 +1,15 @@
+#### Bivariate (bi)linear interpolation  --- on a grid ---
+#### -----------------------------------------------------
+#### The more general situation is handled by the algorithms in the
+#### 'akima' package
+
+## approx(x, y = NULL, xout, method = "linear", n = 50,
+##        yleft, yright, rule = 1, f = 0, ties = mean)
+approx2 <- function(x)
+{
+  ## Purpose:
+  ## ----------------------------------------------------------------------
+  ## Arguments:
+  ## ----------------------------------------------------------------------
+  ## Author: Martin Maechler, Date: 14 May 2011, 20:11
+}
