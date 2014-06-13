e75379d16ebd52960022bdb4edd56519197cd13c
Martin Maechler <maechler@r-project.org>
2014-06-13 16:36:26 +0000

    version of 2014.06.13.16.36.26 (now is 09 Aug 2014).

--- R/p.res.2x.WSt.R
+++ R/p.res.2x.WSt.R
@@ -11,7 +11,9 @@
 ##    p.res.2x(...)
 ## }
 
-p.res.2x <-
+p.res.2x <- function(x, ...) UseMethod("p.res.2x")
+
+p.res.2x.default <-
   function(x, y, z, restricted = NULL, size = 1, slwd = 1, scol = 2:3,
            xlab = NULL, ylab = NULL, main = NULL,
            xlim = range(x), ylim = range(y), ...)
@@ -61,13 +63,75 @@ p.res.2x <-
   invisible()
 }
 
+## graphics:::mosaicplot.formula as an example
+p.res.2x.formula <- function(x = ~., data,
+                             main = deparse(substitute(data)),
+                             xlab = NULL, ylab = NULL, ...)
+{
+    ## Purpose:  plot residuals vs. two x's
+    ## Author:   ARu , Date:  11/Jun/91
+    ## Aenderungen: MMae, 30/Jan/92, Dez.94 / WSt
+    ## --------------------------------------------------------------------------
+    ## Arguments:
+    ##   x         formula defining the variables zu be used, either
+    ##             z ~ x + y
+    ##             ~ x + y   in this case, data must inherit from  lm ,
+    ##             and the residuals of  data  will be used as  z .
+    ##   data      a data.frame or an  lm  or  aov  object.
+    ##             In the latter case, p.res.2x will look for the data
+    ##             that was used to fit the model.
+    ##  restricted      absolute value which truncates the size.
+    ##             The corresponding symbols are marked by stars.
+    ##  size       the symbols are scaled so that 'size' is the size of
+    ##             the largest symbol in cm.
+    ##  slwd, scol line width and color to be used for the symbols
+    ##  ...        additional arguments for the S-function 'plot'
+    ## EXAMPLE :
+    ## g.res2x(zz~.,data=data.frame(xx=rep(1:10,7),yy=rep(1:7, rep(10,7)),
+    ##    zz=rnorm(70)), restr = 2, main = "i.i.d.  N(0,1) random residuals")
+    ## --------------------------------------------------------------------------
+    if(miss.main <- missing(main))
+	force(main)
+    formula <- as.formula(x)
+    t.d <- if(inherits(data, "lm")) {
+	if(miss.main) main <- paste0("residuals(", main, ")")
+	if(!is.data.frame(t.d <- data$model)) {
+	    ## try to look for the data that was used to fit the model.
+	    cl <- data$call
+	    i <- if("data" %in% names(cl)) "data" else 3 # try ..
+	    t.d <- get(as.character(cl[[i]]))
+	}
+	if (length(formula) < 3) {
+	    if(identical(format(formula), "~.")) formula <- formula(data)
+	    formula <- update.formula(formula, residuals ~ .)
+	    ## formula <- substitute(residuals ~ RHS, list(RHS = formula[[2]]))
+	    cbind(t.d, residuals =  residuals(data))
+	} else t.d
+    } else
+	data
+    if (!is.data.frame(t.d)) {
+	if(is.matrix(data)) data <- as.data.frame(data) else
+	stop("data is not a data frame or 'lm' object with 'model' or existing data")
+    }
+    t.d <- na.omit(model.frame(formula, t.d))
+    z <- t.d[,1]
+    x <- t.d[,2]; if(is.null(xlab)) xlab <- names(t.d)[2]
+    y <- t.d[,3]; if(is.null(ylab)) ylab <- names(t.d)[3]
+    if(is.factor(x) && is.factor(y))
+	p.res.2fact(x, y, z, main=main, xlab=xlab, ylab=ylab, ...)
+    else {
+	x <- as.numeric(t.d[,2])
+	y <- as.numeric(t.d[,3])
+	p.res.2x.default(x,y,z, main=main, xlab=xlab, ylab=ylab, ...)
+    }
+}
 
 p.res.2fact <-
     function(x, y, z, restricted, notch = FALSE,
              xlab = NULL, ylab = NULL, main = NULL)
 {
-    if(is.null(xlab)) xlab <- deparse (substitute (x))
-    if(is.null(ylab)) ylab <- deparse (substitute (y))
+    if(is.null(xlab)) xlab <- deparse(substitute(x))
+    if(is.null(ylab)) ylab <- deparse(substitute(y))
     if(is.null(main)) main <- deparse(substitute(z))
 
     ok <- !(is.na(x) | is.na(y) | is.na(z))
@@ -88,7 +152,8 @@ p.res.2fact <-
         }
     }
     rz <- range(z)
-    par (mfrow = c(length(ly), 1), oma = c(5,6,6,0), mar = .1 + c(2,4,0,1))
+    op <- par(mfrow = c(length(ly), 1), oma = c(5,6,6,0), mar = .1 + c(2,4,0,1))
+    on.exit(par(op))
     for (yv in rev(ly)) {
         Ind <- y == yv
         plot (x[Ind], z[Ind], ylim = rz, xlab = "", ylab = yv, notch = notch)
@@ -104,7 +169,7 @@ p.res.2fact <-
     mtext (xlab, side = 1, line = 1, outer = TRUE, cex = 1.3)
     mtext (ylab, side = 2, line = 3, outer = TRUE, cex = 1.3)
     mtext (main, side = 3, line = 2, cex = 1.5, outer = TRUE)
-    if(any(restr)) cat(sum(restr), "restricted observation(s)\n")
+    if(any(restr)) message(sum(restr), " restricted observation(s)")
     invisible()
 }
 
