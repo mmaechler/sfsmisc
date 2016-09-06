36d90ddd4789a3265299c5e652339a87cb1bba8c
Martin Maechler <maechler@r-project.org>
2016-09-06 11:37:36 +0200

    sessionInfoX() now also reports capabilities() etc
    Additionally, depending on the version of R,
    La_version(), extSoftVersion(), and pcre_config() as well.

--- R/sessionInfo-ext.R
+++ R/sessionInfo-ext.R
@@ -3,8 +3,14 @@ sessionInfoX <- function(pkgs=NULL, list.libP = FALSE, extraR.env = TRUE) {
     if(!is.null(pkgs)) stopifnot(is.character(pkgs), length(pkgs) > 0)
     lP <- .libPaths() # *is* normalized in the sense of normalizePath()
     nRL <- normalizePath(RLIBS <- strsplit(Sys.getenv("R_LIBS"), ":")[[1]])
+    si <- sessionInfo()
+    Rver <- package_version(si$R.version)
     structure(class = "sessionInfoX",
-        list(sInfo = sessionInfo(),
+        list(sInfo = si,
+	     capabilities = capabilities(),
+	     extSoft = if(Rver >= "3.2.0") extSoftVersion(),
+	     LAPACK  = if(Rver >= "3.0.3") La_version(),
+	     pcre    = if(Rver >= "3.1.3") pcre_config(),
              pkgDescr = if(!is.null(pkgs)) sapply(pkgs, packageDescription, simplify=FALSE),
              libPath = lP, .Library = .Library, RLIBS = RLIBS, n.RLIBS = nRL,
              list.libP = if(list.libP) sapply(lP, list.files, simplify=FALSE),
@@ -15,7 +21,7 @@ sessionInfoX <- function(pkgs=NULL, list.libP = FALSE, extraR.env = TRUE) {
              })))
 }
 
-print.sessionInfoX <- function(x, ...) {
+print.sessionInfoX <- function(x, locale = TRUE, ...) {
     cat("Extended  sessionInfo():",
         "-----------------------", sep="\n")# does add a final '\n'
     if(!is.null(pkgD <- x$pkgDescr)) {
@@ -23,22 +29,34 @@ print.sessionInfoX <- function(x, ...) {
         print(pkgD, ...)
         cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
     }
-    cat("R_LIBS:\n")
+    cat("Capabilities:\n")
+    print(symnum(x$capabilities, symbols = c("-", "X")), ...)
+
+    if(!is.null(LA <- x$LAPACK)) cat("\nLAPACK version:", LA, "\n")
+    if(!is.null(extS <- x$extSoft)) {
+        cat("External software (versions):")
+        es <- cbind(version = unlist(x$extSoft)); names(dimnames(es)) <- c("name", "")
+        print(noquote(es), ...)
+    }
+
+    cat("\nR_LIBS:\n")
     cbind(x$RLIBS)
     xtr.lp <- setdiff(x$libPath,
                       union(normalizePath(x$.Library), x$n.RLIBS))
     if(length(xtr.lp)) {
-        cat("libPath contents in addition to RLIBS and .Library:\n")
+        cat("libPath [.libPaths()] contents in addition to R_LIBS and .Library:\n")
         print(xtr.lp)
     } else
-        cat("libPath contains RLIBS and .Library (normalized)\n")
+        cat("libPath contains not more than RLIBS and .Library (normalized)\n")
     if(length(xx <- setdiff(x$n.RLIBS, x$libPath))) { ## typically empty
         cat("** RLIBS has entries not in .libPaths():\n")
         print(xx)
     }
-    cat("Main R env. variables (for more, inspect the 'xR.env' component):\n")
+    cat("Main R env. variables",
+        if(!is.null(x$xR.env)) " (for more, inspect the 'xR.env' component)",
+        ":\n", sep="")
     print(cbind(x$R.env), ...)
     cat("---------------- standard sessionInfo():\n")
-    print(x$sInf, ...)
+    print(x$sInf, locale=locale, ...)
     invisible(x)
 }
