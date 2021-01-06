f9dea9a515135ab385d80a331107b45f38e58104
Martin Maechler <maechler@r-project.org>
2021-01-06 21:40:27 +0100

    new  pkgDesc() and pkgBuilt()

---
+++ R/pkgDesc.R
@@ -0,0 +1,10 @@
+##' A slightly more convenient version of packageDescription()
+##' 1. returns named character vector; no other attributes
+pkgDesc <- function (pkg, lib.loc = NULL, fields = NULL, ...) {
+    pd <- packageDescription(pkg, lib.loc=lib.loc, fields=fields, drop=FALSE, ...)
+    file <- attr(pd, "file")
+    structure(class = "Dlist", c(unlist(pd), c(file=file)))
+}
+
+##' useful: e.g. as  sapply( pkgs, pkgBuilt)
+pkgBuilt <- function(pkg, lib.loc = NULL, ...) pkgDesc(pkg, lib.loc=lib.loc, fields = "Built", ...)
