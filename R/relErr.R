fb3515b411e1766d06ff38ce4dfea411eabcc94c
Martin Maechler <maechler@r-project.org>
2021-09-10 16:24:33 +0200

    relErrV() enhancements

--- R/relErr.R
+++ R/relErr.R
@@ -12,9 +12,19 @@ relErr <- function(target, current) { ## make this work, also for 'Matrix' ==> n
 relErrV <- function(target, current, eps0 = .Machine$double.xmin) {
     n <- length(target <- as.vector(target))
     ## assert( <length current> is multiple of <length target>) :
-    if(length(current) %% n)
+    lc <- length(current)
+    if(!n) {
+	if(!lc) return(numeric()) # everything length 0
+	else stop("length(target) == 0 differing from length(current)")
+    } else if(!lc)
+	stop("length(current) == 0 differing from length(target)")
+    ## else n, lc  > 0
+    if(lc %% n)
 	stop("length(current) must be a multiple of length(target)")
-    R <- target # (possibly "mpfr")
+    R <- if(lc != n) # explicitly recycle
+	     target[rep(seq_len(n), length.out=lc)]
+	 else
+	     target # (possibly "mpfr")
     R[] <- 0
     ## use *absolute* error when target is zero {and deal with NAs}:
     t0 <- abs(target) < eps0 & !(na.t <- is.na(target))
--- R/sessionInfo-ext.R
+++ R/sessionInfo-ext.R
@@ -111,7 +111,7 @@ shortRversion <- function(Rv = R.version,
         if(Rst == "Under development (unstable)")
             ## "R Under development (unstable) (2017-10-16 r73554)"
             paste("R devel", sub(paste0(".*",pat), "\\1", Rvstring))
-        else if(tolower(Rst) %in% c("patched", "alpha", "beta", "RC"))
+        else if(tolower(Rst) %in% c("patched", "alpha", "beta", "rc"))
             ## "R version 3.4.2 Patched (2017-10-12 r73556)"
             sub(pat, "\\1", sub(" version", "", Rvstring))
         else if(Rst == "") # "R version 3.2.5 (2016-04-14)"  (regular release)
